c7dbd3f57ad7f9c5cd67a5808dd4ec71c1e8d227
Filip Simic <fsimic346@gmail.com>
2024-09-28 13:12:19 +0200

    Fixed navbar title alignment and button animation

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'cw-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
