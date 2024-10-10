8726f5f035124d60f75d6aa4439e06b03c278b99
Marko Čekerinac <marko.cekerinac@gmail.com>
2024-10-10 20:07:02 +0200

    similar items

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -1,15 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Design } from '../../models/design';
 import { DesignService } from '../design.service';
 import { CartService } from '../cart.service';
 import { CartItem } from '../../models/cartItem';
 import { CommonModule } from '@angular/common';
+import { HomeCardComponent } from "../home-card/home-card.component";
 
 @Component({
   selector: 'cw-design',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HomeCardComponent],
   templateUrl: './design.component.html',
   styleUrl: './design.component.scss',
 })
@@ -18,19 +19,24 @@ export class DesignComponent implements OnInit {
   selectedImage: any;
   sizes: string[] = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
   selectedSize: string = this.sizes[0];
-
+  id:string=''
   route: ActivatedRoute = inject(ActivatedRoute);
   designService: DesignService = inject(DesignService);
   cartService: CartService = inject(CartService);
+  contentRendered: boolean = false;
+  shirtDesigns: Design[] = [];
+  hoodieDesigns: Design[] = [];
+  changeDetector = inject(ChangeDetectorRef);
 
   async ngOnInit(): Promise<void> {
-    const id = this.route.snapshot.paramMap.get('id') as string;
+    this.id = this.route.snapshot.paramMap.get('id') as string;
     this.designService
-      .getDesign(id)
-      .then((design) => {
+      .getDesign(this.id)
+      .then(async (design) => {
         if (design) {
           this.design = design;
           this.selectedImage = this.design.image;
+          await this.loadDesigns();
         } else {
           console.error('Design not found');
         }
@@ -38,6 +44,41 @@ export class DesignComponent implements OnInit {
       .catch((error) => {
         console.error('Error fetching design:', error);
       });
+      
+  }
+
+  async loadDesigns(): Promise<void> {
+    if (this.design!.type === 'Majica') {
+      const shirts = await this.designService.getDesigns({
+        count: 5,
+        type: 'Majica',
+        // category: this.design!.category.toLowerCase(),
+      });
+  
+      this.shirtDesigns = shirts.filter(shirt => shirt.id !== this.id);
+    }
+  
+    if (this.design!.type === 'Duks') {
+      const hoodies = await this.designService.getDesigns({
+        count: 5,
+        type: 'Duks',
+        // category: this.design!.category.toLowerCase(),
+      });
+  
+      this.hoodieDesigns = hoodies.filter(hoodie => hoodie.id !== this.id);
+    }
+  }
+  
+
+  onContentRendered() {
+    setTimeout(() => {
+      this.contentRendered = true;
+      this.changeDetector.detectChanges();
+    }, 0);
+  }
+
+  identify(index: number, design: Design) {
+    return design.id;
   }
 
   selectSize(size: string) {
