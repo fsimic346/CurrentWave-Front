9446cb5e59e5ee9ae122a8f34f100227a3a0ca34
Filip Simic <fsimic346@gmail.com>
2024-10-13 18:11:42 +0200

    Added cart page and hamburger menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DesignComponent } from './design/design.component';
 import { ProductsPageComponent } from './products-page/products-page.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'design/:id', component: DesignComponent },
   { path: 'products', component: ProductsPageComponent },
+  { path: 'cart', component: CartComponent },
 ];
